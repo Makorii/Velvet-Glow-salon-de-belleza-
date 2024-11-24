97625ba41819f0701c6ddd526b2ca32384dc71ec
Marcela Duran <marcelaromina.d@hotmail.com>
2024-11-24 19:08:23 -0300

    agrego imagenes, color y medidas a las cards de servicios

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
-// import Box from '@mui/material/Box';
 import Container from "@mui/material/Container";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -8,7 +7,9 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid2";
+import { CardActionArea} from "@mui/material";
 
 export default function Home() {
   return (
@@ -82,22 +83,35 @@ export default function Home() {
           />
         </Card>
       </Container>
-      <Box component="section" padding="20px 64px" sx={{backgroundColor:"white"}}>
-        <Typography align="center" color="black" variant="h4">Promos del mes</Typography>
-        <Card sx={{ display: "flex", padding: "20px 0", backgroundColor:"white", color: "black" }}>
+      <Box
+        component="section"
+        padding="20px 64px"
+        sx={{ backgroundColor: "white" }}
+      >
+        <Typography align="center" color="black" variant="h4">
+          Promos del mes
+        </Typography>
+        <Card
+          sx={{
+            display: "flex",
+            padding: "20px 0",
+            backgroundColor: "white",
+            color: "black",
+          }}
+        >
           <CardMedia
             component="img"
             height="200px"
             image="/src/assets/image.jpg"
             alt="mujer cortandose el cabello"
-            sx={{ width: "250px"}}
+            sx={{ width: "250px" }}
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
-            Combo de Belleza Express
+              Combo de Belleza Express
             </Typography>
-            <Typography variant="body2" sx={{ color:"black" }}>
-            Corte de cabello + Secado + Manicura express.
+            <Typography variant="body2" sx={{ color: "black" }}>
+              Corte de cabello + Secado + Manicura express.
             </Typography>
             <CardActions
               sx={{
@@ -114,7 +128,7 @@ export default function Home() {
                   borderRadius: "16px",
                   fontWeight: 600,
                   border: "1px solid #E5A65E",
-                  textTransform: "none"
+                  textTransform: "none",
                 }}
               >
                 ¡Quiero un turno!
@@ -122,20 +136,28 @@ export default function Home() {
             </CardActions>
           </CardContent>
         </Card>
-        <Card sx={{ display: "flex", padding: "20px 0", backgroundColor:"white", color: "black" }}>
-        <CardMedia
+        <Card
+          sx={{
+            display: "flex",
+            padding: "20px 0",
+            backgroundColor: "white",
+            color: "black",
+          }}
+        >
+          <CardMedia
             component="img"
             height="200px"
             image="/src/assets/peinado.jpg"
             alt="mujer peinandose el cabello"
-            sx={{ width: "250px", objectFit: "cover"}}
+            sx={{ width: "250px", objectFit: "cover" }}
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
-            Combo Fiesta
+              Combo Fiesta
             </Typography>
-            <Typography variant="body2" sx={{ color:"black" }}>
-            Peinado especial + Maquillaje profesional + Manicura con decoración.
+            <Typography variant="body2" sx={{ color: "black" }}>
+              Peinado especial + Maquillaje profesional + Manicura con
+              decoración.
             </Typography>
             <CardActions
               sx={{
@@ -152,7 +174,7 @@ export default function Home() {
                   borderRadius: "16px",
                   fontWeight: 600,
                   border: "1px solid #E5A65E",
-                  textTransform: "none"
+                  textTransform: "none",
                 }}
               >
                 ¡Quiero un turno!
@@ -160,21 +182,28 @@ export default function Home() {
             </CardActions>
           </CardContent>
         </Card>
-        <Card sx={{ display: "flex", padding: "20px 0", backgroundColor:"white", color: "black" }}>
-        <CardMedia
+        <Card
+          sx={{
+            display: "flex",
+            padding: "20px 0",
+            backgroundColor: "white",
+            color: "black",
+          }}
+        >
+          <CardMedia
             component="img"
             height="200px"
             image="/src/assets/image2.png"
             alt="mujer recibiendo tratamiento facial"
-            sx={{ width: "250px"}}
+            sx={{ width: "250px" }}
           />
-          
+
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
-            Promo “Día de Amigas”
+              Promo “Día de Amigas”
             </Typography>
-            <Typography variant="body2" sx={{ color:"black" }}>
-            Peinado + Manicura + Tratamiento facial para 2 personas.
+            <Typography variant="body2" sx={{ color: "black" }}>
+              Peinado + Manicura + Tratamiento facial para 2 personas.
             </Typography>
             <CardActions
               sx={{
@@ -191,7 +220,7 @@ export default function Home() {
                   borderRadius: "16px",
                   fontWeight: 600,
                   border: "1px solid #E5A65E",
-                  textTransform: "none"
+                  textTransform: "none",
                 }}
               >
                 ¡Quiero un turno!
@@ -200,7 +229,102 @@ export default function Home() {
           </CardContent>
         </Card>
       </Box>
-
+      <Box
+        component="section"
+        padding="20px 64px"
+        sx={{ backgroundColor: "white" }}
+      >
+        <Typography align="center" color="black" variant="h4">
+          Servicios
+        </Typography>
+        <Grid container spacing={6}>
+          <Grid size={6}>
+          <Card sx={{backgroundColor:"#F3CBBF", color:"black"}}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="350px"
+                  image="/src/assets/cuidadocabello.jpg"
+                  alt="cuidado del cuerpo"
+                  sx={{
+                    objectFit: "cover",
+                    width: "100%",
+                    height: { xs: "202px",sm: "300px", md: "350px"}
+                  }}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div" align="center">
+                    Cuidado del cabello
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid size={6}>
+            <Card sx={{backgroundColor:"#F3CBBF", color:"black"}}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="350px"
+                  image="/src/assets/cuidadocuerpo.jpg"
+                  alt="cuidado del cuerpo"
+                  sx={{
+                    objectFit: "cover",
+                    width: "100%",
+                  }}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div" align="center">
+                    Cuidado del cuerpo
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid size={6}>
+          <Card sx={{backgroundColor:"#F3CBBF", color:"black"}}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="350px"
+                  image="/src/assets/cuidadounias.jpg"
+                  alt="cuidado del cuerpo"
+                  sx={{
+                    objectFit: "cover",
+                    width: "100%",
+                  }}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div" align="center">
+                    Cuidado de las uñas
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid size={6}>
+          <Card sx={{backgroundColor:"#F3CBBF", color:"black"}}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="350px"
+                  image="/src/assets/cuidadopiel.jpg"
+                  alt="cuidado del cuerpo"
+                  sx={{
+                    objectFit: "cover",
+                    width: "100%",
+                  }}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div" align="center">
+                    Cuidado de la piel
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        </Grid>
+      </Box>
     </React.Fragment>
   );
 }
